cc5ca93635f9fa87ac179d87305ff7518ce563bc
derailed <fernand.galiana@gmail.com>
2018-09-09 12:23:36 -0600

    updates

--- http/greeter/cmd/greet_svc.go
+++ http/greeter/cmd/greet_svc.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gopherland/labs/http/greeter"
 )
 
 const httpPort = ":4500"
 
 func main() {
-	// YOUR CODE...
+	greeter.SetupRoutes()
 
 	log.Printf("Dial a Greeter is listening [%s]", httpPort)
 	log.Fatal(http.ListenAndServe(httpPort, nil))
--- http/greeter/routes.go
+++ http/greeter/routes.go
@@ -1,7 +1,10 @@
 package greeter
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 // Response greeting message format.
@@ -18,10 +21,35 @@ func SetupRoutes() {
 
 // GreetingHandler greats incoming users.
 func GreetingHandler(w http.ResponseWriter, r *http.Request) {
-	// YOUR CODE...
+	name := r.URL.Query().Get("name")
+	age, err := strconv.Atoi(r.URL.Query().Get("age"))
+	if err != nil {
+		http.Error(w, "invalid age specified", http.StatusExpectationFailed)
+		return
+	}
+
+	msg, err := greet(name, age)
+	if err != nil {
+		http.Error(w, "you must provide a user name", http.StatusExpectationFailed)
+		return
+	}
+
+	resp := Response{
+		Greeting: msg,
+		Name:     name,
+		Age:      age,
+	}
+	buff, err := json.Marshal(&resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(w, string(buff))
 }
 
 // NoMatchHandler is used when no known endpoints.
 func NoMatchHandler(w http.ResponseWriter, r *http.Request) {
-	// YOUR CODE...
+	http.Error(w, fmt.Sprintf("no matching routes for %s", r.URL), http.StatusBadRequest)
 }
