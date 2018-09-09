4e1d9798e5103784067de38b5c13fb598345c7c6
derailed <fernand.galiana@gmail.com>
2018-09-09 12:11:33 -0600

    updates

--- benchmarking/greeter/greeter.go
+++ benchmarking/greeter/greeter.go
@@ -1,6 +1,12 @@
+// Greeter computes a greeting message
+
 package greeter
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func greeter1(n string, a int) string {
 	const format = "Hello, %s! You are %d old today..."
@@ -8,11 +14,19 @@ func greeter1(n string, a int) string {
 }
 
 func greeter2(n string, a int) string {
-	// YOUR_CODE
-	return ""
+	return "Hello, " + n + "! You are " + strconv.Itoa(a) + " old today..."
 }
 
 func greeter3(n string, a int) string {
-	// YOUR_CODE
-	return ""
+	var b strings.Builder
+
+	b.Grow(39)
+	// fmt.Fprintf(&b, "Hello, "+n+"! You are "+strconv.Itoa(a)+" old today...")
+	b.WriteString("Hello, ")
+	b.WriteString(n)
+	b.WriteString("! You are ")
+	b.WriteString(strconv.Itoa(a))
+	b.WriteString(" old today...")
+
+	return b.String()
 }
